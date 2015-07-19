c288062d89c6cf9faa570fcf88a3ad7ed6d6c969
edwardsCNC <nicholas.lee.edwards@gmail.com>
2015-07-19 10:57:06 -0600

    Separated miscellaneous program preparation and completion logic into a CNCHelper class

--- Face_Mill.rb
+++ Face_Mill.rb
@@ -1,4 +1,5 @@
-require './rectangle.rb'
+require './Rectangle.rb'
+require './CNCHelper.rb'
 
 #explain the program
 	puts ""
@@ -48,47 +49,10 @@ puts ""
 puts "Here is your CNC program:"
 puts ""
 
-#safety block including absolute mode, canned cycle cancellation, and cutter radius compensation cancellation
-puts "G90G80G40"
+helper = CNCHelper.new(units, tool_number, tool_radius, radial_depth_of_cut, feedrate, spindle_speed, safe_height)
+helper.program_before_face_milling
 
-#block with preparatory command for units selection
-if units == "metric"
-	puts "G21"
+rectangle = Rectangle.new(width, height, tool_radius, radial_depth_of_cut)
+rectangle.generate_face_milling_toolpath
 
-	elsif units == "imperial"
-		  puts "G20"
-
-	else #default to metric in case of inexact match between user input and logical condition
-		puts "G21"
-
-end
-
-#tool selection
-puts "T#{tool_number}M06"
-
-#assumes approach in +X direction from -X position
-#rapid feed to safe position before machining
-puts "G0X#{(-2*tool_radius).round(3)}Y#{(-2*tool_radius).round(3)}"
-
-#create block specifying spindle speed. start the spindle. start coolant flow.
-puts "S#{spindle_speed}M03M08"
-
-#create block for feed to position of first cut. include the feedrate.
-puts "G1X#{(-tool_radius + radial_depth_of_cut).round(3)}Y#{(-tool_radius + radial_depth_of_cut).round(3)}F#{feedrate.round(3)}"
-
-Rectangle.new(width, height, tool_radius, radial_depth_of_cut).generate_face_milling_toolpath
-
-#retract the tool to a safe height above the work surface
-puts "G91G0Z#{safe_height}"
-puts "G90"
-
-#turn off the spindle and coolant 
-puts "M04M09"
-
-#return to X0Y0
-puts "G0X0Y0"
-
-#end the program
-puts "M30"
-
-puts ""
+helper.program_after_face_milling
