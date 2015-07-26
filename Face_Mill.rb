a87603879b87d6a6fdb64d5757b57a6b2d0346bd
edwardsCNC <nicholas.lee.edwards@gmail.com>
2015-07-25 23:03:18 -0600

    Added a line of spacing between each line of user input in Face_Mill.rb for clarity and aesthetics. Added a brief introduction and a reference to the README for CNC machine setup instructions.

--- Face_Mill.rb
+++ Face_Mill.rb
@@ -2,41 +2,56 @@ require './Rectangle.rb'
 require './CNCHelper.rb'
 
 #get user input
+	
+	puts ""
+	puts ""
+	puts ""
+	puts "Face_Mill is a CNC programming utility that creates a climb milling CNC program which can be used to machine a rectangular flat surface."
+	puts ""
+	puts "For CNC machine setup instructions, refer to https://github.com/edwardsCNC/Face_Mill/blob/master/README.md"
 	puts ""
 	puts "You will now be prompted to input specifications of your desired program."
 	puts ""
 
 	puts "Will the units of the program be metric (millimeters) or imperial (inches)? Type and enter \"metric\" or \"imperial\". An inexact input will result in a default setting of metric units."
 	units = String(gets.chomp)
+	puts ""
 
 	puts "What is the length of the material to be face-milled in the X direction?"
 	width = Float(gets)
+	puts ""
 
 	puts "What is the length of the material to be face-milled in the Y direction?"
 	height = Float(gets)
+	puts ""
 
 	puts "What is the ATC number of the tool to be used? (Be sure to include a zero before a single digit tool number; e.g. 01 instead of 1)"
 	tool_number = String(gets.chomp)
+	puts ""
 
 	puts "What is the tool radius?"
 	tool_radius = Float(gets)
+	puts ""
 
 	puts "What is the radial depth of cut?"
 	radial_depth_of_cut = Float(gets)
+	puts ""
 
 	puts "What is the feedrate?"
 	feedrate = Float(gets)
+	puts ""
 
 	puts "What is the spindle speed?"
 	spindle_speed = Float(gets)
+	puts ""
 
 	puts "At the end of the program, the tool will retract to a safe distance above the material before rapid motion to X0Y0."
 	puts "What is the height of the safe distance?"
 	safe_height = Float(gets)
+	puts ""
 
-puts ""
-puts "Here is your CNC program:"
-puts ""
+	puts "Here is your CNC program:"
+	puts ""
 
 helper = CNCHelper.new(units, tool_number, tool_radius, radial_depth_of_cut, feedrate, spindle_speed, safe_height)
 helper.program_before_face_milling
