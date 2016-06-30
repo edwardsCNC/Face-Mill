dcb05b2801a8e300babe5f3235c15db2ebf246f4
edwardsCNC <nicholas.lee.edwards@gmail.com>
2016-06-29 20:39:23 -0600

    Eliminate duplication of test data instantiation

--- spec/cnc_helper_spec.rb
+++ spec/cnc_helper_spec.rb
@@ -2,29 +2,26 @@ require '../lib/CNCHelper.rb'
 require './spec_helper.rb'
 
 RSpec.describe CNCHelper do
+
+  let(:helper) { CNCHelper.new(0.125, 0.225, 15.0, 0.25) }
 	
   it "generates a rapid feed toolpath to a safe starting position of negative 2x the tool radius along the X and Y axes" do
-    helper = CNCHelper.new(0.125, 0.225, 15.0, 0.25)
     expect {helper.program_before_face_milling}.to output(/G0X-0.25Y-0.25\n/).to_stdout	
   end
 
   it "generates a feed toolpath to the position of the first cut" do 
-  	helper = CNCHelper.new(0.125, 0.225, 15.0, 0.25)
   	expect {helper.program_before_face_milling}.to output(/G1X0.1Y0.1/).to_stdout
   end
 
   it "completes the initial feed command with the feedrate specified by the user" do
-  	helper = CNCHelper.new(0.125, 0.225, 15.0, 0.25)
   	expect {helper.program_before_face_milling}.to output(/F15.0\n/).to_stdout
   end
 
   it "generates a rapid feed toolpath to retract the tool to a safe height" do
-    helper = CNCHelper.new(0.125, 0.225, 15.0, 0.25)
     expect {helper.program_after_face_milling}.to output(/G91G0Z0.25\nG90/).to_stdout
   end
 
   it "generates a rapid feed toolpath to return the tool to X0.0Y0.0" do
-    helper = CNCHelper.new(0.125, 0.225, 15.0, 0.25)
     expect {helper.program_after_face_milling}.to output(/G0X0.0Y0.0/).to_stdout
   end
 
--- spec/rectangle_spec.rb
+++ spec/rectangle_spec.rb
@@ -3,8 +3,9 @@ require './spec_helper.rb'
 
 RSpec.describe Rectangle do
 
+	let(:rectangle) { Rectangle.new(0.5, 0.5, 0.125, 0.225) }
+
 	it "generates a face-milling toolpath which mills the entire flat surface specified by the user" do
-		rectangle = Rectangle.new(0.5, 0.5, 0.125, 0.225)
 		expect {rectangle.generate_face_milling_toolpath}.to output("G1Y0.4\nG1X0.4\nG1Y0.1\nG1X0.325\nG1Y0.175\n").to_stdout
 	end
 
