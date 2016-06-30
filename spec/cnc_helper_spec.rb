0fbca8a7cc965891a14fca8f8a0eea24d050c045
edwardsCNC <nicholas.lee.edwards@gmail.com>
2016-06-29 19:35:03 -0600

    Unit test for CNCHelper: generate a rapid feed toolpath to a safe starting position

--- spec/cnc_helper_spec.rb
+++ spec/cnc_helper_spec.rb
@@ -3,6 +3,9 @@ require './spec_helper.rb'
 
 RSpec.describe CNCHelper do
 	
-#put tests here
+  it "generates a rapid feed toolpath to a safe starting position of negative 2x the tool radius along the X and Y axes" do
+    helper = CNCHelper.new(0.125, 0.225, 15.0, 0.25)
+    expect {helper.program_before_face_milling}.to output(/G0X-0.25Y-0.25\n/).to_stdout	
+  end
 
 end
